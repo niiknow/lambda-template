8c329ee3d2389cc65a12ffbc301e8e65d80e59d4
noogen <tom@noogen.net>
2018-09-16 23:38:24 -0500

    completed

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
-import RemoteView from './lib/RemoteView'
+import RemoteViews from './lib/RemoteViews'
 
 export const template = async (event, context, callback) => {
-  const view = new RemoteView(event.path.tenant, event.body);
+  const view = new RemoteViews(event.path.tenant, event.body);
   const response = {
     statusCode: 200,
     body: await view.render(),
--- tests/handler.test.js
+++ tests/handler.test.js
@@ -1,5 +1,7 @@
 import * as handler from '../handler';
 
+require('debug').enabled('*');
+
 test('template', async () => {
   const event = {
   	path: {
@@ -7,7 +9,11 @@ test('template', async () => {
   	},
   	body: {
       template: {
-        url: 'https://'
+        url: 'https://raw.githubusercontent.com/niiknow/serverless-template/master/tests/views/test1.dot'
+      },
+      state: {
+        name: 'john',
+        age: 100
       }
   	}
   };
@@ -15,7 +21,8 @@ test('template', async () => {
   const context = 'context';
   const callback = (error, response) => {
     expect(response.statusCode).toEqual(200);
-    expect(typeof response.body).toBe("string");
+    expect(typeof response.body).toBe('string');
+    expect(response.body).toEqual('<html><head><title>hi</title></head><body class=\"hi\"><div>Hi john!</div><div>100</div></body></html>');
   };
 
   await handler.template(event, context, callback);
