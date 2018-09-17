70c17258e1f875988cd6f86705eac41068ea9514
noogen <tom@noogen.net>
2018-09-17 15:31:13 -0500

    allow two step render

--- handler.js
+++ handler.js
@@ -2,7 +2,26 @@ import ViewEngine from './lib/ViewEngine'
 
 export const template = async (event, context, callback) => {
   const viewng = new ViewEngine(event.path.bucket);
-  const response = {
+  let response = {
+    statusCode: 200
+  };
+
+  // two step process, slower
+  if (event.method === 'GET') {
+  	if (event.query.url) {
+	  let item = {};
+	  // retrieve remote config
+	  await viewng.getJsonItem('body', event.query.url, item);
+	  event.body = item.body;
+	} else {
+	  let response = {
+	    statusCode: 422,
+	    body: 'The query string parameter "url" of your config json is required.'
+	  };
+	}
+  }
+
+  response = {
     statusCode: 200,
     body: await viewng.render(event.body),
   };
